aa26be83ad6ea4b4aa4668a715050f7f69d8f538
muhamedshiyad <muhamedshiyad18@gmail.com>
2025-01-23 16:52:40 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connctDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.get("/products", async (req, res) => {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({sucess: false, message: "All fields are required"});
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({sucess: true, data:newProduct});
+    } catch (error) {
+        console.error("Error in create product:",error.message);
+        res.status(500).json({sucess: false, message:"server Error"});
+    }
+});
 
 
 app.listen(5000, () => {
