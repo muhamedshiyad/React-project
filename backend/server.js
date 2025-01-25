ec33c00dc8c316536b89000f06998164be235f4a
muhamedshiyad <muhamedshiyad18@gmail.com>
2025-01-25 14:41:56 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connctDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+
+import productRoutes from "./routes/productroutes.js";
 
 dotenv.config();
 
@@ -9,34 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/api/products", async (req, res) => {
-    const product = req.body;
-
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({sucess: false, message: "All fields are required"});
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({sucess: true, data:newProduct});
-    } catch (error) {
-        console.error("Error in create product:",error.message);
-        res.status(500).json({sucess: false, message:"server Error"});
-    }
-});
-
-app.delete("/api/products/:id",async (req, res) => {
-    const {id} = req.params
-
-   try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({sucess: true,message:"Product deleted successfully"});
-   } catch (error) {
-    res.status(404).json({sucess: false, message:"product not found"});
-   }
-});
+app.use("/api/products",productRoutes);
 
 app.listen(5000, () => {
     connctDB();
